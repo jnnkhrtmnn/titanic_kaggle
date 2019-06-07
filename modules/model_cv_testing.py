2ccad58a9f50564e019880dfecda7ffd04c3d3ef
Jannik Hartmann <jannik.hartmann@outlook.de>
2019-06-07 09:50:32 +0200

    minor changes

--- modules/model_cv_testing.py
+++ modules/model_cv_testing.py
@@ -12,8 +12,8 @@ def cv_testing(X, y, model, cv=5):
     from sklearn.model_selection import cross_val_score
 
 
-    acc = (cross_val_score(model, X, y, cv=5, scoring='accuracy'))
-    brier = (cross_val_score(model, X, y, cv=5, scoring='brier_score_loss'))
+    acc = (cross_val_score(model, X, y, cv=cv, scoring='accuracy'))
+    brier = (cross_val_score(model, X, y, cv=cv, scoring='brier_score_loss'))
     
     print(" Mean of Accs: %s" % (np.mean(acc)))
     print(" Std of Accs: %s" % (np.std(acc)))
